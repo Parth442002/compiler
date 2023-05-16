56afae8041352b1804ff7d23d9bfd1ac23f7f59a
Parth442002 <parthkatiyar04@gmail.com>
2023-05-16 08:47:08 +0530

    first ✅

--- main.py
+++ main.py
@@ -47,3 +47,9 @@ async def leftf():
 @app.get("/firstf")
 async def firstFollow():
     return {"code": firstFollowCode}
+
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
